c7f42d2f58ed7a1b79d90afa351e927c950dcd5b
SoonWookHwang <108779266+SoonWookHwang@users.noreply.github.com>
2025-07-29 09:32:08 +0900

    FIX: web security config 인가 코드 수정

--- src/main/java/com/coubee/coubeebegateway/config/WebSecurityConfig.java
+++ src/main/java/com/coubee/coubeebegateway/config/WebSecurityConfig.java
@@ -50,7 +50,9 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(registry -> registry
                         .requestMatchers("/api/user/auth/**").permitAll()
                         .requestMatchers("/api/store/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/api/store/su").hasRole("SUPER_ADMIN")
+                        .requestMatchers("/api/store/su/**").hasRole("SUPER_ADMIN")
+                        .requestMatchers("/api/product/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/product/su/**").hasRole("SUPER_ADMIN")
                         .anyRequest().authenticated()
                 );
 
