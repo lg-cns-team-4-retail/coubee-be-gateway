4919c9e4e7026f7c7929aafcff2dee710c988f47
SoonWookHwang <108779266+SoonWookHwang@users.noreply.github.com>
2025-08-27 16:31:07 +0900

    Fix: report 관련 api endpoint 변경

--- src/main/java/com/coubee/coubeebegateway/config/WebSecurityConfig.java
+++ src/main/java/com/coubee/coubeebegateway/config/WebSecurityConfig.java
@@ -65,7 +65,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/store/su/**").hasRole("SUPER_ADMIN")
                         .requestMatchers("/api/product/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/product/su/**").hasRole("SUPER_ADMIN")
-                        .requestMatchers("/api/reports/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/order/reports/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 );
         return http.build();
